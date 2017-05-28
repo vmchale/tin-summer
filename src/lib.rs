68e4b27573a778690ddbe5bd465af75f789e9614
Vanessa McHale <tmchale@wisc.edu>
2017-05-28 04:03:32 -0500

    permissions fix?

--- src/lib.rs
+++ src/lib.rs
@@ -19,48 +19,51 @@ fn is_artifact(p: PathBuf, re: Option<Regex>) -> bool {
 }
 
 pub fn read_files(in_paths: &PathBuf, depth: u8, min_bytes: Option<u64>) -> FileTree {
-    let paths = fs::read_dir(&in_paths).unwrap();
     let mut tree = FileTree::new();
     let mut total_size = FileSize::new(0);
 
-    for p in paths {
-        let path = p.unwrap().path(); // TODO no unwraps b/c broken symlinks
+    if let Some(paths) = fs::read_dir(&in_paths) {
+        for p in paths {
+            let path = p.unwrap().path(); // TODO no unwraps b/c broken symlinks
 
-        // if this fails, it's probably because `path` is a symlink, so we ignore it.
-        if let Ok(metadata) = fs::metadata(&path) {
-            // append file size/name for a file
-            if metadata.is_file() {
-                let file_size = FileSize::new(metadata.len());
-                if let Some(b) = min_bytes {
-                    if file_size >= FileSize::new(b) {
-                        tree.push(path.clone(), file_size, None, depth + 1);
+            // if this fails, it's probably because `path` is a symlink, so we ignore it.
+            if let Ok(metadata) = fs::metadata(&path) {
+                // append file size/name for a file
+                if metadata.is_file() {
+                    let file_size = FileSize::new(metadata.len());
+                    if let Some(b) = min_bytes {
+                        if file_size >= FileSize::new(b) {
+                            tree.push(path.clone(), file_size, None, depth + 1);
+                        }
                     }
+                    else {
+                        tree.push(path, file_size, None, depth + 1);
+                    }
+                    total_size.add(file_size);
                 }
-                else {
-                    tree.push(path, file_size, None, depth + 1);
-                }
-                total_size.add(file_size);
-            }
 
-            // otherwise, go deeper
-            else if metadata.is_dir() {
-                let mut subtree = read_files(&path, depth + 1, min_bytes);
-                let dir_size = subtree.file_size;
-                if let Some(b) = min_bytes {
-                    if dir_size >= FileSize::new(b) {
+                // otherwise, go deeper
+                else if metadata.is_dir() {
+                    let mut subtree = read_files(&path, depth + 1, min_bytes);
+                    let dir_size = subtree.file_size;
+                    if let Some(b) = min_bytes {
+                        if dir_size >= FileSize::new(b) {
+                            tree.push(path, dir_size, Some(&mut subtree), depth + 1);
+                        }
+                    }
+                    else {
                         tree.push(path, dir_size, Some(&mut subtree), depth + 1);
                     }
+                    total_size.add(dir_size);
                 }
-                else {
-                    tree.push(path, dir_size, Some(&mut subtree), depth + 1);
-                }
-                total_size.add(dir_size);
+            }
+            else {
+                println!("{}: ignoring symlink at {}", "Warning".yellow(), path.display());
             }
         }
-        else {
-            println!("{}: ignoring symlink at {}", "Warning".yellow(), path.display());
-        }
+    }
+    else {
+        println!("{}: permission denied for directory", "Warning".yellow());
     }
     tree
 }
-
