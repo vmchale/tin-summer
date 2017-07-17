3fac6a8cc950f52f18cce761cb007b0609d33a23
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-07-16 19:22:20 -0500

    fix for travis ci

--- ci/script.sh
+++ ci/script.sh
@@ -2,7 +2,6 @@
 
 set -ex
 
-# TODO This is the "test phase", tweak it as you see fit
 main() {
     cross build --target $TARGET
     cross build --target $TARGET --release
@@ -14,8 +13,8 @@ main() {
     cross test --target $TARGET
     cross test --target $TARGET --release
 
-    cross run --target $TARGET
-    cross run --target $TARGET --release
+    cross run --target $TARGET -- a
+    cross run --target $TARGET --release -- a
 }
 
 # we don't run the "test phase" when doing deploys
