dfa988fce1cf89540f9049186a23f4e3919394f9
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-11-27 07:09:43 -0600

    fancy travis

--- ci/script.sh
+++ ci/script.sh
@@ -3,18 +3,23 @@
 set -ex
 
 main() {
-    cross build --target "$TARGET"
-    cross build --target "$TARGET" --release
+    if [ "$TARGET" = "x86_64-unknown-redox" ]; then
+        rustup target add x86_64-unknown-redox
+        cargo build --target "$TARGET" --release
+    else
+        cross build --target "$TARGET"
+        cross build --target "$TARGET" --release
 
-    if [ ! -z "$DISABLE_TESTS" ]; then
-        return
+        if [ ! -z "$DISABLE_TESTS" ]; then
+            return
+        fi
+
+        cross test --target "$TARGET"
+        cross test --target "$TARGET" --release
+
+        cross run --target "$TARGET" -- a
+        cross run --target "$TARGET" --release -- a
     fi
-
-    cross test --target "$TARGET"
-    cross test --target "$TARGET" --release
-
-    cross run --target "$TARGET" -- a
-    cross run --target "$TARGET" --release -- a
 }
 
 # we don't run the "test phase" when doing deploys
