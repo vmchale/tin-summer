74dc3579047048ce5bbda9a21b838f692a76a5a2
Vanessa McHale <vanessa.mchale@reconfigure.io>
2018-02-02 23:21:21 -0600

    setup fancy rules

--- build.rs
+++ build.rs
@@ -13,6 +13,7 @@ use clap::App;
 pub const MAN_SN: &'static str = include_str!("man/tin-summer.1");
 
 fn main() {
+    setup_thefuck();
 
     setup_manpages(MAN_SN, "tin-summer");
 
@@ -26,11 +27,11 @@ fn main() {
     let mut app = App::from_yaml(yaml).version(crate_version!());
 
     // generate bash completions if desired
-    #[cfg(feature = "bash")] app.gen_completions("sn", Shell::Bash, env!("BASH_COMPLETIONS_DIR"));
+    #[cfg(feature = "bash")]
+    app.gen_completions("sn", Shell::Bash, env!("BASH_COMPLETIONS_DIR"));
 
     // generate fish completions if desired
     let mut app_snd = App::from_yaml(yaml).version(crate_version!());
     #[cfg(feature = "fish")]
     app_snd.gen_completions("sn", Shell::Fish, env!("FISH_COMPLETIONS_DIR"));
-
 }
--- src/utils.rs
+++ src/utils.rs
@@ -12,12 +12,16 @@ use std::fs::Metadata;
 use std::os::linux::fs::MetadataExt;
 
 #[cfg(any(target_os = "macos", target_os = "freebsd", target_os = "netbsd",
-            target_os = "dragonfly", target_os = "solaris"))]
+          target_os = "dragonfly", target_os = "solaris"))]
 use std::os::unix::fs::MetadataExt;
 
 #[cfg(target_os = "linux")]
 pub fn size(m: &Metadata, blocks: bool) -> u64 {
-    if blocks { m.st_blocks() * 512 } else { m.len() }
+    if blocks {
+        m.st_blocks() * 512
+    } else {
+        m.len()
+    }
 }
 
 #[cfg(any(target_os = "windows", target_os = "redox"))]
@@ -26,10 +30,10 @@ pub fn size(m: &Metadata, _: bool) -> u64 {
 }
 
 #[cfg(any(target_os = "macos", target_os = "freebsd", target_os = "netbsd",
-            target_os = "dragonfly", target_os = "solaris"))]
+          target_os = "dragonfly", target_os = "solaris"))]
 pub fn size(m: &Metadata, blocks: bool) -> u64 {
     if blocks {
-        m.blocks() * 512 // idk if this is correct on bsd/linux
+        m.blocks() * 512 // idk if this is correct on bsd
     } else {
         m.len()
     }
@@ -38,44 +42,34 @@ pub fn size(m: &Metadata, blocks: bool) -> u64 {
 /// Gather the information from `.gitignore`, `.ignore`, and darcs `boring` files in a given
 /// directory, and assemble a `RegexSet` from it.
 pub fn mk_ignores(in_paths: &PathBuf, maybe_ignore: &Option<RegexSet>) -> Option<RegexSet> {
-
     if let Some(ref ignore) = *maybe_ignore {
         Some(ignore.to_owned())
-    } else if let (ignore_path, Ok(mut file)) =
-        {
-            let mut ignore_path = in_paths.clone();
-            ignore_path.push(".ignore");
-            (ignore_path.clone(), File::open(ignore_path.clone()))
-        }
-    {
+    } else if let (ignore_path, Ok(mut file)) = {
+        let mut ignore_path = in_paths.clone();
+        ignore_path.push(".ignore");
+        (ignore_path.clone(), File::open(ignore_path.clone()))
+    } {
         let mut contents = String::new();
-        file.read_to_string(&mut contents).expect(
-            "File read failed.",
-        ); // ok because we check that the file exists
+        file.read_to_string(&mut contents)
+            .expect("File read failed."); // ok because we check that the file exists
         Some(file_contents_to_regex(&contents, &ignore_path))
-    } else if let (gitignore_path, Ok(mut file)) =
-        {
-            let mut gitignore_path = in_paths.clone();
-            gitignore_path.push(".gitignore");
-            (gitignore_path.clone(), File::open(gitignore_path))
-        }
-    {
+    } else if let (gitignore_path, Ok(mut file)) = {
+        let mut gitignore_path = in_paths.clone();
+        gitignore_path.push(".gitignore");
+        (gitignore_path.clone(), File::open(gitignore_path))
+    } {
         let mut contents = String::new();
-        file.read_to_string(&mut contents).expect(
-            "File read failed.",
-        ); // ok because we check that the file exists
+        file.read_to_string(&mut contents)
+            .expect("File read failed."); // ok because we check that the file exists
         Some(file_contents_to_regex(&contents, &gitignore_path))
-    } else if let (darcs_path, Ok(mut file)) =
-        {
-            let mut darcs_path = in_paths.clone();
-            darcs_path.push("_darcs/prefs/boring");
-            (darcs_path.clone(), File::open(darcs_path))
-        }
-    {
+    } else if let (darcs_path, Ok(mut file)) = {
+        let mut darcs_path = in_paths.clone();
+        darcs_path.push("_darcs/prefs/boring");
+        (darcs_path.clone(), File::open(darcs_path))
+    } {
         let mut contents = String::new();
-        file.read_to_string(&mut contents).expect(
-            "File read failed.",
-        ); // ok because we check that the file exists
+        file.read_to_string(&mut contents)
+            .expect("File read failed."); // ok because we check that the file exists
         Some(darcs_contents_to_regex(&contents, &darcs_path))
     } else {
         None
@@ -86,5 +80,9 @@ pub fn mk_ignores(in_paths: &PathBuf, maybe_ignore: &Option<RegexSet>) -> Option
 /// the spawning counts as one OS thread.
 pub fn get_processors() -> usize {
     let n = get();
-    if n > 1 { n - 1 } else { n }
+    if n > 1 {
+        n - 1
+    } else {
+        n
+    }
 }
