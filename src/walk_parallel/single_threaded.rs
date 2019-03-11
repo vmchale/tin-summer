d3e83fc0e129c4a715d233568043438ef2624492
Roy Ivy III <rivy.dev@gmail.com>
2019-03-10 19:29:11 -0500

    fix is_artifact() logic (docs == 'executable *and* included in .gitignore')

--- src/walk_parallel/single_threaded.rs
+++ src/walk_parallel/single_threaded.rs
@@ -195,7 +195,7 @@ pub fn is_artifact(
         if REGEX.is_match(path_str) || (path_str == "tags" && vimtags) {
             true
         } else if let Some(ref x) = *gitignore {
-            if is_executable(path_str, metadata) || REGEX_GITIGNORE.is_match(path_str) {
+            if is_executable(path_str, metadata) && REGEX_GITIGNORE.is_match(path_str) {
                 x.is_match(full_path)
             } else {
                 false
